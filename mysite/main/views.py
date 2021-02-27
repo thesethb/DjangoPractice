37b0ef64d67663f2d25361f593facca76dcfcb7f
thesethb <67613424+thesethb@users.noreply.github.com>
2021-02-27 17:47:43 -0500

    Tim Video Templates

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,9 +4,11 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
+
 def index(response, id):
     ls = ToDoList.objects.get(id = id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name,str(item.text)))
+    return render(response, "main/base.html",{})
+
+
 def home(response):
-    pass
+    return render(response, "main/home.html", {})
