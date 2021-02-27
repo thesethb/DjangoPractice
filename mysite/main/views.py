b735d47dc85c3a399ee4b663201b61313beb115f
thesethb <67613424+thesethb@users.noreply.github.com>
2021-02-27 16:29:34 -0500

    Here

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>my name is, my name is, my name is</h1>")
-def v1(response):
-    return HttpResponse("<h1>hello</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id = id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name,str(item.text)))
