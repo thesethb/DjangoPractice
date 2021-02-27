d511ad0fbb4594eaaf72d11feafa69343a54a51a
thesethb <67613424+thesethb@users.noreply.github.com>
2021-02-27 18:22:49 -0500

    Tim Video Templates

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,7 +7,7 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id = id)
-    return render(response, "main/base.html",{})
+    return render(response, "main/list.html",{'ls':ls})
 
 
 def home(response):
