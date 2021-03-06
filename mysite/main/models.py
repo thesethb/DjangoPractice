7c469a31d94a7d2579a5a43b471fcef3e776ebd5
andrew-bregman <andrew.bregman@westpoint.edu>
2021-03-06 10:21:07 -0500

    commit on 'master'

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -14,3 +14,89 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
+
+        from django.db import models
+import datetime
+from partial_date import PartialDate
+from django import forms
+from phone_field import PhoneField
+
+
+class userID(models.Model):
+    username = models.CharField(max_length=25, null=False, blank=False, unique=True)
+    firstName = models.CharField(max_length = 25, null=False, blank=False)
+    lastName =  models.CharField(max_length = 25, null=False, blank=False)
+    email =  models.EmailField()
+    #pas = models.CharField(max_length=50, null=False, blank=False)
+    #class UserForm(forms.ModelForm):
+    #password = forms.CharField(widget=forms.PasswordInput)
+    #phone = PhoneField(null=False, blank=False, unique=True, help_text='Contact phone number')
+    phone = models.IntegerField(null=False, blank=False)
+    #gradYear = models.IntegerField(_('year'), validators=[MinValueValidator(1984), max_value_current_year])
+    #gradYear = PartialDate("2023")
+    #gradYear.format('%Y')
+    gradYear = models.DateField()
+    company = models.CharField(max_length = 2)
+
+  
+    def __str__(self):
+	    return self.username
+    def __str__(self):
+        return self.firstName
+    def __str__(self):
+        return self.lastName
+ 
+ 
+
+class uBio(models.Model):
+    user = models.ForeignKey(userID, on_delete=models.CASCADE)
+    meetMe = models.CharField(max_length=150, null=False, blank=False)
+    department = models.CharField(max_length = 100, null=False, blank=False)
+    title = models.CharField(max_length = 30, null=False, blank=False) 
+    major = models.CharField(max_length = 50, null=False, blank=False)
+    minor = models.CharField(max_length = 50, null=False, blank=False)
+    aInterest = models.TextField()
+    aExpertise = models.TextField()
+    rGoal = models.TextField()
+    bPic = models.ImageField(height_field='396', width_field='1584')
+    uPic = models.ImageField(height_field='320', width_field='320')
+    curLooking = models.TextField()
+    twitter = models.CharField(max_length = 30, null=False, blank=False)
+    insta = models.CharField(max_length = 30, null=False, blank=False)
+    facebook = models.CharField(max_length = 30, null=False, blank=False)
+    linkedin = models.CharField(max_length = 30, null=False, blank=False)
+    tictok = models.CharField(max_length = 30, null=False, blank=False)
+
+    def __str__(self):
+        return self.meetMe
+    def __str__(self):
+        return self.department
+    def __str__(self):
+        return self.title
+    def __str__(self):
+        return self.major
+    def __str__(self):
+        return self.minor
+    def __str__(self):
+        return self.twitter
+    def __str__(self):
+        return self.insta
+    def __str__(self):
+        return self.facebook
+    def __str__(self):
+        return self.linkedin
+    def __str__(self):
+        return self.tictok
+        
+
+class uProjects(models.Model):
+    user = models.ForeignKey(userID, on_delete=models.CASCADE)
+    #waiting on relational tables to import data from there
+
+ #andrew_user = userID(username="andrewbregman", firstName="Andrew", lastName="bregman", email="andrew.bregman@westpoint.edu", phone=8563832480, gradYear="2023-5-23", company="D2")
+ #andrew_user = userID( )
+ #andrew_user=userID()
+ #andrew_user=userID()
+ #andrew_user=userID()
+ #andrew_user=userID()
+ #andrew_user.save()
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -32,6 +32,7 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
+    response.user
     if response.method == "POST":
         form = CreateNewList(response.POST)
 
