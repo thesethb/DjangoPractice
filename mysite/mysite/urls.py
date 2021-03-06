72289d5c8edaf53635e1d3a78123978349f840fd
thesethb <67613424+thesethb@users.noreply.github.com>
2021-03-06 09:45:12 -0500

    Tim Video login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',v.register, name='register'),
     path('', include('main.urls')),
+    path('', include("django.contrib.auth.urls")),
 ]
 # /home/start
